dd05f8090e06402fb6d04cad5126eedbe8c1b9e1
Uday Deshmukh <udaypawar004@gmail.com>
2025-01-09 11:08:42 +0530

    Solution of Level 1 Program 1

---
+++ HarryAge.java
@@ -0,0 +1,9 @@
+public class HarryAge{
+	public static void main(String[] args) {
+		int harryBirthYear = 2000;
+		int currentYear = 2024;
+		int harryAge = currentYear - harryBirthYear;
+		
+		System.out.println("Harry's age is " + harryAge);
+	}
+}
